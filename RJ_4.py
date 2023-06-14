10130483891d6676b8663eeab12074f619d7577f
yui-m0294 <iyr50855@kwansei.ac.jp>
2023-06-14 16:47:13 +0900

    Initial commit

---
+++ RJ_4.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry("500x1000")
+
+canvas = tk.Canvas(root, bg = "white")
+canvas.pack(fill = tk.BOTH, expand = True)
+
+canvas.create_line(150, 200, 150, 300, fill = "black")
+canvas.create_line(150, 250, 50, 210, fill = "black")
+canvas.create_line(150, 250, 250, 210, fill = "black")
+canvas.create_line(150, 300, 50, 400, fill = "black")
+canvas.create_line(150, 300, 250, 400, fill = "black")
+canvas.create_oval(100,100,200,200,fill='white')
+root.mainloop()
