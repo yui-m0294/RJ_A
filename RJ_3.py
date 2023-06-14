d33c187c7c9d3d45c2bb0c5a94b492662d353d8a
yui-m0294 <iyr50855@kwansei.ac.jp>
2023-06-14 16:35:43 +0900

    Initial commit

---
+++ RJ_3.py
@@ -0,0 +1,12 @@
+import shutil
+shutil.unpack_archive('sample.zip', '')
+input_folder = "./sample"
+file_name = "kitamura"
+
+sum = 0
+for i in range(1,1000,2):
+    f = open('{}/{}_{:0>5d}_kgu.txt'.format(input_folder,file_name,i))
+    print(i)
+    data = f.read()
+    sum += int(data)
+print(sum) 
